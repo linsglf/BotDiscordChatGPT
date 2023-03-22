fd16a950d55c33155e7d3a3349c88515c12dfcbb
Gabriel Lins <gabriellf.glf@gmail.com>
2023-03-22 07:01:07 -0300

    Update api_openai.py
    Comentário na API do OpenAI para mudar o tipo de resposta gerada pelo chat

--- BotDiscordChatGPT/openai_api/api_openai.py
+++ BotDiscordChatGPT/openai_api/api_openai.py
@@ -17,3 +17,21 @@ def get_chat_gpt_response(question: str) -> str:
     image_url = response_from_chat_gpt['data'][0]['url']
 
     return image_url
+
+'''
+Para usar o chat gpt com perguntas e respostas, use o código abaixo:
+
+def get_chat_gpt_response(question: str) -> str:
+    response_from_chat_gpt = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=question,
+        max_tokens=100,
+    )
+
+    if response_from_chat_gpt:
+        choices: List[Dict[str, Any]] = response_from_chat_gpt.get("choices", None)
+        if choices and len(choices) > 0:
+            text: str = choices[0].get("text", None)
+            return text
+    return None
+'''
